1df20cd6399dbabcde1936f1eeabe9408fc338b6
io01Demo <132198888+io01Demo@users.noreply.github.com>
2023-12-14 21:34:14 +0200

    Update nvd.sh

--- nvd.sh
+++ nvd.sh
@@ -4,11 +4,12 @@
 base_url="https://services.nvd.nist.gov/rest/json/cves/2.0?"
 vendor="$1"
 product="$2"
+filename="$3"
 api_key="b9eca5af-dac0-473b-9405-29b96a4976cd"
 
 # Validate parameters
 if [[ -z "$vendor" || -z "$product" ]]; then
-  echo "Error: Missing required parameters. Usage: $0 <vendor> <product>"
+  echo "Error: Missing required parameters. Usage: $0 <vendor> <product> <outputfile>"
   exit 1
 fi
 
@@ -24,17 +25,23 @@ keywordSearch_url="keywordSearch=$vendor"
 query_url="$base_url$keywordSearch_url"   # search based on keywords 
 
 # https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch=Siemens
-msg="Running query- $query_url"
-echo $msg
+
 headers="-H \"apiKey: $api_key\""
 
 
 # Make API call using curl
 
-curl $query_url -o "NVD-CVE.json"
+curl $query_url -o $filename
 #cves=$(curl -sSL "$query_url" $headers)
 #cves=$(curl -sSL "$query_url" )
-cat NVD-CVE.JSON | jq 
+cat $filename | jq ".vulnerabilities "
+cat $filename | jq  ".vulnerabilities " > "CVE" + $filename #JSON format
+#rm $filename
+echo "Relavant CVE's"
+echo "---------------------------"
+cat "CVE" + $filemame |grep "CVE" 
+msg="Query- $query_url"
+echo $msg
 echo "For detailed information about each CVE, please visit the NVD website: https://nvd.nist.gov/vuln/detail Or contact IO01 team"
 
 exit 0
